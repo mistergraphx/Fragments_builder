22dbc2b68d8fb43e42d24b60e9cec0e9ce829af2
Mist. GraphX <arnaud.berard@mister-graphx.com>
2016-02-14 11:00:45 +0100

    Swig datas
    app datas are stored in datas/app.json

--- gulp-tasks/swig.js
+++ gulp-tasks/swig.js
@@ -1,12 +1,12 @@
 /** # gulp-swig
 
 - load all .twig templates at the root of the project.srcPtah/templates folder -> pages
-- template inherance : all the _layouts.twig are placed in the /layout folder
-- organised partials in the /partials folder
+- template inherance : all the _layouts.twig are placed in the /_layout folder
+- organised partials in the /_partials folder
 - Load a page_name.json if it's found in the /datas folder
-- load basics form the app.json file find in the project.BasePath
+- load basics form the /datas/app.json file find in the datas folder
 - Render Markdown with | markdown filter or {% markdown%} {% endmarkdown %}
-- Copy the rendered html files in the root of the project.srcPath
+- Render html files in the root of the project.srcPath
 
 https://www.npmjs.org/package/gulp-swig
 https://www.npmjs.org/package/gulp-data
@@ -23,8 +23,7 @@ https://help.github.com/articles/github-flavored-markdown/
 module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onError, marked){
     
     return function(){
-        
-        //console.log(marked);
+
         var markedConfig = marked.configure({
                 gfm: true,
                 tables: true,
@@ -34,8 +33,6 @@ module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onErr
                 smartLists: true,
                 smartypants: false
         });
-        
-        
         
         var opts = {
             load_json: true,
@@ -47,7 +44,7 @@ module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onErr
                 }
             },
             data: {
-                app : require('../'+project.BasePath+'app.json') ,
+                app : require('../'+project.SrcPath+'datas/app.json') ,
                 summary: require('../'+project.SrcPath+'datas/summary.json'),
             },
             setup: function(swig) {
@@ -61,14 +58,11 @@ module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onErr
                     errorHandler: onError
             }))
             .pipe(plugins.swig(opts))
-            //.pipe(plugins.cached('Swig'))
             .pipe(gulp.dest(project.SrcPath))
             .pipe(plugins.notify({
                 title: "SWIG TASK",
                 message: "Swig/Html is rendered"
             }))
             .on("end", browserSync.reload);
-        
-
     }
 };
