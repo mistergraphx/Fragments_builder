a7e3e150e4d8e20b197bcca7f3aca82b90153ab4
Mist. GraphX <arnaud.berard@mister-graphx.com>
2016-02-15 19:22:10 +0100

    update package json
    - add licence, author, blabla …
    - update dev dependencies
    - change gulp-combine-media-queries to gulp-group-css-media-queries

--- gulp-tasks/combineMQ.js
+++ gulp-tasks/combineMQ.js
@@ -1,22 +1,18 @@
 /** # combineMQ
 
-@requires   gulp-combine-media-queries
-@see        https://www.npmjs.com/package/gulp-combine-media-queries
-
-@see  Autre source : https://github.com/SE7ENSKY/group-css-media-queries
+@requires   gulp-group-css-media-queries
+@see        https://www.npmjs.com/package/gulp-group-css-media-queries
+@see        https://github.com/SE7ENSKY/group-css-media-queries
 
 */
 module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onError){
     return function(){
+        
         gulp.src(project.SrcPath+project.cssPath+'**/*.css')
             .pipe(plugins.plumber({
                 errorHandler: onError
             }))
-            .pipe(plugins.combineMediaQueries({
-                    log: true,
-                    use_external: false
-                })
-            )
+            .pipe(plugins.groupCssMediaQueries())
         .pipe(gulp.dest(project.DevPath+project.cssPath))
         .pipe(plugins.notify('MediaQueries combined !'));
     }
