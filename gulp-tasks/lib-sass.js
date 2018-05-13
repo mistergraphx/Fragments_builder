d5b16f22eaeb57eaa3672f87d50183a44f3a2311
Mist. GraphX <arnaud.berard@mister-graphx.com>
2018-05-13 13:37:54 +0200

    maj sur les sourcemaps path, ne rétabli pas le fonctionnement des dev tools sous chrome

--- gulp-tasks/lib-sass.js
+++ gulp-tasks/lib-sass.js
@@ -22,25 +22,25 @@ on peut spécifier un autre chemin dans le fichier de config du projet
 
 */
 module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onError) {
-    
+
     // on initialise la variable par default pour le chemin des sourcemaps
     // par defaut vide pour les sourcemaps inlines
     var sourcemapsPath;
-       
+
     if(process.env.NODE_ENV === 'production'){
         sourcemapsPath = './';
-    }   
-    
+    }
+
     // Test si la config du projet a cette propriétées de definie
     if(project.hasOwnProperty("sourcemaps")
        && project.hasOwnProperty("sourcemaps.path")
        && project.sourcemaps !== 'undefined') {
             sourcemapsPath = project.sourcemaps.path;
     }
-    
-    
+
+
     // console.log('SourceMap Path : ' + sourcemapsPath);
-    
+
     return function (){
         gulp.src(project.SrcPath + project.sassPath+'**/*.scss')
             // Error Handler
@@ -48,9 +48,13 @@ module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onErr
                 errorHandler: onError
             }))
             // Sourcemaps initialisation
-            .pipe(sourcemaps.init({
-                debug: true
-            }))
+            .pipe(sourcemaps.init())
+			      // .pipe(sourcemaps.identityMap())
+//            .pipe(sourcemaps.mapSources(function(sourcePath, file) {
+//                // source paths are prefixed with '../src/'
+//                //console.log(sourcePath);
+//				return '../_scss/' + sourcePath;
+//            }))
             // Sass
             .pipe(plugins.sass({
                 errLogToConsole: true,
@@ -59,16 +63,30 @@ module.exports = function(gulp, plugins, project, sourcemaps, browserSync, onErr
                 //sourceMap:'sass',
                 includePaths : project.includePaths
             }))
-            // Source map
-            // ----------
-            // Ici on écrit la sourcemap par défaut dans le fichier
-            // ceci permet de pouvoir avoir un chemin d'accès correct sous chrome
-            // comme expliqué [ici](http://www.sitepoint.com/simple-gulpy-workflow-sass/)
-            // conflit avec Suzy : https://github.com/floridoo/gulp-sourcemaps/issues/144#issuecomment-229044077
-            //
-            // Si besoin on peut spécifier un chemin, et des options supplémentaires pour une sourcemap externe
-            // cf : https://www.npmjs.com/package/gulp-sourcemaps
-            .pipe(sourcemaps.write(sourcemapsPath))
+
+			//Source map
+			//----------
+			//Problemes de chemins dans chrome
+			//Exemple de tche gulp qui démontre les problème de chmins induis par l'enchainement de
+			//pipes() sass|sourcemaps|autoprefixer
+			//il semblerais que autoprefixer fausse le chemin des sources
+			//https://github.com/ByScripts/gulp-sample/blob/master/gulpfile.js
+			//
+			//Ici on écrit la sourcemap par défaut dans le fichier
+			//ceci permet de pouvoir avoir un chemin d'accès correct sous chrome
+			//comme expliqué [ici](http://www.sitepoint.com/simple-gulpy-workflow-sass/)
+			//conflit avec Suzy : https://github.com/floridoo/gulp-sourcemaps/issues/144#issuecomment-229044077
+			//
+			//Si besoin on peut spécifier un chemin, et des options supplémentaires pour une sourcemap externe
+			//cf : https://www.npmjs.com/package/gulp-sourcemaps
+			//Fri Oct 27 15:48:38 2017 - chrome a l'air de mieux supporter les sourcemaps externe on  génère donc un fichier .map externe
+			//@todo vérifier le passage de la config depuis le projet
+            .pipe(sourcemaps.write('./', {
+      				//includeContent: false,
+      				sourceRoot: '../_scss',
+      				destPath: project.bundleConfig.dest ,
+      				//sourceMappingURLprefix: project.bundleConfig.dest + 'assets/css'
+      			}))
             // Output
             // ------
             //.pipe(gulp.dest(project.DevPath+project.cssPath))  // /!\ Attention on copie en Dev alors que Autoprefixer et combine mediaQueries ne sont pas passés
